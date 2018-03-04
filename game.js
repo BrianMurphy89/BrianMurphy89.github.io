cd2ac59bffa37a91f77300489f992e7058776a91
Brian R Murphy <BrianRMurphy89@gmail.com>
2018-03-04 16:52:22 -0500

    Completed random number push into new array

--- game.js
+++ game.js
@@ -7,6 +7,7 @@ const ace = 14;
 
 
 const cardArray = [2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,jack,jack,jack,jack,queen,queen,queen,queen,king,king,king,king,ace,ace,ace,ace];
+const randomlySelectedNum = [];
 
 $(() => {
 
@@ -21,11 +22,28 @@ $(() => {
   //Display PlayerTwo input and repeat the process.
   /////////////Objectives/////////////////////
 
-const $button = $('#Start');
+const $button = $('#start');
+const $playerOne = $('#playerOne');
+const $playerTwo = $('#playerTwo');
+const $playerOneInput = $('#playerOneInput');
+const $playerOneSubmit = $('#playerOneSubmit');
+const $randomizeNum = cardArray[Math.floor(Math.random() * cardArray.length)]
 $button.on('click', () => {
   //Need something to display image of the back of a card
-  cardArray[Math.floor(Math.random() * cardArray.length)];
-
+  $playerTwo.hide()
+  randomlySelectedNum.push($randomizeNum)
+  console.log(randomlySelectedNum);
   })
 
+  $playerOneSubmit.on('click', () => {
+  if ($playerOneInput.val() === randomlySelectedNum[0]) {
+    alert('You are correct!');
+    $playerOne.hide();
+    $playerTwo.show();
+  } else if ($playerOneInput.val() < randomlySelectedNum[0]) {
+      alert('The card is higher! You have one more guess!')
+  } else {
+      alert('The card is lower! You have one more guess!')
+    }
+  })
 })
