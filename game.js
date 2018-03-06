c52555fccf69623d72435b20758f15b8f5b606a9
Brian R Murphy <BrianRMurphy89@gmail.com>
2018-03-05 22:19:02 -0500

    Need to fix card generator to randomize cards on every turn.

--- game.js
+++ game.js
@@ -8,7 +8,7 @@ const ace = 14;
 
 const cardArray = [2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,jack,jack,jack,jack,queen,queen,queen,queen,king,king,king,king,ace,ace,ace,ace];
 const randomlySelectedNum = [];
-const $randomizeNum = Math.floor(Math.random() * cardArray.length)
+// const $randomizeNum = Math.floor(Math.random() * cardArray.length)
 
 
 $(() => {
@@ -29,11 +29,26 @@ const $playerOne = $('#playerOne');
 const $playerTwo = $('#playerTwo');
 const $playerOneInput = $('#playerOneInput');
 const $playerOneSubmit = $('#playerOneSubmit');
+const $playerTwoInput = $('#playerTwoInput');
+const $playerTwoSubmit = $('#playerTwoSubmit');
+let $secondGuess = false;
+let $playerOneFinal = 0;
+let $playerTwoFinal = 0;
+const cardNum = cardArray[$randomizeNum]
+//Card Generator is holding 1 value and not randomizing a new card
+const cardGenerator = () => {
+  console.log(cardNum);
+  let $randomizeNum = Math.floor(Math.random() * cardArray.length)
+  randomlySelectedNum.push(cardNum)
+  cardArray.splice($randomizeNum,1);
+  console.log(randomlySelectedNum);
+}
+
+
 $button.on('click', () => {
   //Need something to display image of the back of a card
   $playerTwo.hide()
-  const cardNum = cardArray[$randomizeNum]
-  randomlySelectedNum.push(cardNum)
+  cardGenerator();
   // console.log(cardNum);
   // console.log(randomlySelectedNum);
   })
@@ -56,17 +71,73 @@ const playerOneCardEval = () => {
 
   $playerOneSubmit.on('click', () => {
     const faceCardEval = playerOneCardEval();
-    console.log(faceCardEval);
+    // console.log(faceCardEval);
     console.log(randomlySelectedNum);
     console.log(randomlySelectedNum[randomlySelectedNum.length -1]);
   if (faceCardEval == randomlySelectedNum[randomlySelectedNum.length - 1]) {
     alert('You are correct!');
     $playerOne.hide();
+    cardGenerator();
     $playerTwo.show();
-  } else if (faceCardEval < randomlySelectedNum[randomlySelectedNum.length -1]) {
+    alert('Player Two - You\'re Up!')
+  } else if (faceCardEval < randomlySelectedNum[randomlySelectedNum.length -1] && $secondGuess == false) {
       alert('The card is higher! You have one more guess!')
-  } else {
+      $secondGuess = true;
+  } else if (faceCardEval > randomlySelectedNum[randomlySelectedNum.length -1] && $secondGuess == false){
+      $secondGuess = true;
       alert('The card is lower! You have one more guess!')
+  } else if ($secondGuess) {
+    let drinkTime = Math.abs(randomlySelectedNum[randomlySelectedNum.length -1] - faceCardEval)
+    alert('Your last guess was wrong! You need to drink for ' + drinkTime + ' seconds!')
+      $playerOneFinal += drinkTime;
+      $playerOne.hide();
+      cardGenerator();
+      $playerTwo.show();
+      return $playerOneFinal;
+  }
+  })
+
+  const playerTwoCardEval = () => {
+    if(typeof($playerTwoInput.val()) === "number") {
+      return $playerTwoInput.val();
+    }  else if ($playerTwoInput.val().toLowerCase() == 'jack'){
+      return 11;
+    } else if ($playerTwoInput.val().toLowerCase() == 'queen') {
+      return 12;
+    } else if ($playerTwoInput.val().toLowerCase() == 'king') {
+      return 13;
+    } else if ($playerTwoInput.val().toLowerCase() == 'ace') {
+      return 14;
+    } else {
+      return $playerTwoInput.val();
     }
+  }
+
+  $playerTwoSubmit.on('click', () => {
+    const faceCardEval2 = playerTwoCardEval();
+    // console.log(faceCardEval);
+    console.log(randomlySelectedNum);
+    console.log(randomlySelectedNum[randomlySelectedNum.length -1]);
+  if (faceCardEval2 == randomlySelectedNum[randomlySelectedNum.length - 1]) {
+    alert('You are correct!');
+    $playerTwo.hide();
+    cardGenerator();
+    $playerOne.show();
+    alert('Player One - You\'re Up!')
+  } else if (faceCardEval2 < randomlySelectedNum[randomlySelectedNum.length -1] && $secondGuess == false) {
+      alert('The card is higher! You have one more guess!')
+      $secondGuess = true;
+  } else if (faceCardEval2 > randomlySelectedNum[randomlySelectedNum.length -1] && $secondGuess == false){
+      $secondGuess = true;
+      alert('The card is lower! You have one more guess!')
+  } else if ($secondGuess) {
+    let drinkTime = Math.abs(randomlySelectedNum[randomlySelectedNum.length -1] - faceCardEval2)
+    alert('Your last guess was wrong! You need to drink for ' + drinkTime + ' seconds!')
+      $playerTwoFinal += drinkTime;
+      $playerTwo.hide();
+      cardGenerator();
+      $playerOne.show();
+      return $playerTwoFinal;
+  }
   })
 })
